aac525ea306599e2103019b38d37df21f3ac2ced
Ahmed-Tarek-Mahmoud <143649528+Ahmed-Tarek-Mahmoud@users.noreply.github.com>
2024-11-02 19:24:48 +0200

    last 2 problem on LLs

---
+++ IntersectonOf2LinkedLists.cpp
@@ -0,0 +1,25 @@
+/**
+ * Name: Ahmed Tarek
+ * Date: 2 Nov 2024
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == nullptr || headB == nullptr) return nullptr;
+        ListNode* a = headA;
+        ListNode* b = headB;
+        // loop till finding intersection or one of the lists ends
+        while( a != b){
+            a = a == nullptr? headB : a->next;
+            b = b == nullptr? headA : b->next;    
+        }
+        return a;
+    }
+};
---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,37 @@
+/**
+ * Name: Ahmed Tarek
+ * Date: 2 Nov 2024
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    // recursive solution 
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* prev = nullptr;
+        ListNode* h2 = reverseList(head->next); // my new head
+        head->next->next = head;
+        head->next=prev;
+        return h2;
+    }
+
+    // iterative solution using 3 sliding pointers
+    ListNode* reverseList(ListNode* head) {
+        ListNode *p = head, *q = nullptr, *r = nullptr;
+        while(p != nullptr){
+            r = q;
+            q = p;
+            p = p->next;
+            q->next = r;
+        }
+        // p will be out and q will be standing on the last node
+        return q;
+    }
+};
