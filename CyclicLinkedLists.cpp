c76823d41aeface9e5be8c3fb3a562c0460b28e5
Ahmed-Tarek-Mahmoud <143649528+Ahmed-Tarek-Mahmoud@users.noreply.github.com>
2024-11-02 16:29:03 +0200

    2 problems on LLs

---
+++ CyclicLinkedLists.cpp
@@ -0,0 +1,31 @@
+/**
+ * Name: Ahmed Tarek
+ * Date: 2 Nov 2024
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // in a linear linked list a fast pointer shouldn't meet the slow except 
+        // once initially at the head
+        ListNode * fast , *slow;
+        fast = head;
+        slow = head;
+        
+        // corner case handling single node and empty list 
+        if(head == nullptr || head->next == nullptr) return false; 
+        
+        do{
+            slow = slow->next;
+            fast = fast->next;
+            fast = (fast)?fast->next:nullptr; // if already null so keep it as it is
+        }while(fast && slow && fast != slow);
+        return fast == slow;
+    }
+};
---
+++ MiddleOfLinkedList.cpp
@@ -0,0 +1,28 @@
+/**
+ * Name: Ahmed Tarek
+ * Date: 2 Nov 2024
+ */
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // here we used a technique called slow and fast pointers you can search 
+        // for an animation to know why is that always true
+        ListNode * fast , *slow;
+        fast = head;
+        slow = head;
+        while(fast && fast->next!= nullptr){
+            slow= slow->next;
+            fast= fast->next->next;
+        }
+        return slow;
+    }
+};
